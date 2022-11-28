c227ce5ac48ff2d1d3c7eebd1fbb741ca108dca1
ShawnAbshire <abshire.shawn@gmail.com>
2022-11-28 14:28:18 -0600

    Added amount modifier and a simple cache to stop duplicate problems.

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ const levels = ['easy', 'medium', 'hard'];
 const problems = [];
 const leetcodeProblemUrl = 'https://leetcode.com/problems/';
 const leetcodeApiUrl = 'https://leetcode.com/api/problems/all/';
+const completedProblems = [];
 let totalProblems = 0;
 
 function Problem(problemObject) {
@@ -18,22 +19,34 @@ function Problem(problemObject) {
   this.description = `Problem ID: ${this.id}\nTitle: ${this.title}\nSlug Title: ${this.titleSlug}\nDifficulty: ${this.difficulty}\nIs Paid? ${this.paidOnly}`;
 }
 
-function sendProblem(msg, difficulty) {
+function getProblem(data) {
+	const randProblem = Math.floor(Math.random() * data.length);
+	const problem = data[randProblem];
+	if (completedProblems.includes(problem.id)) {
+		getProblem(data);
+	}
+
+  completedProblems.push(problem.id);
+	return problem;
+}
+
+function sendProblems(msg, difficulty, amount = 1) {
   let data = [...problems];
   if (difficulty != -1) {
     data = data.filter(x => x.difficulty.toLowerCase() === difficulty.toLowerCase());
   }
 
-  const randomProblem = Math.floor(Math.random() * data.length);
-  const problem = data[randomProblem];
-  const problemUrl = `${leetcodeProblemUrl}${problem.titleSlug}/`;
-  const embed = new EmbedBuilder()
-    .setTitle(problem.title)
-    .setColor('#f89f1b')
-    .setThumbnail('https://leetcode.com/static/images/LeetCode_logo_rvs.png')
-    .setDescription(`${problem.difficulty} - ${problem.title}`)
-    .setURL(problemUrl);
-  msg.channel.send({ embeds: [embed]});
+  for (let i = 0; i < amount; i++) {
+    const problem = getProblem(data);
+    const problemUrl = `${leetcodeProblemUrl}${problem.titleSlug}/`;
+    const embed = new EmbedBuilder()
+      .setTitle(problem.title)
+      .setColor('#f89f1b')
+      .setThumbnail('https://leetcode.com/static/images/LeetCode_logo_rvs.png')
+      .setDescription(`${problem.difficulty} - ${problem.title}`)
+      .setURL(problemUrl);
+    msg.channel.send({ embeds: [embed]});
+  }
 }
 
 axios.get(leetcodeApiUrl)
@@ -53,23 +66,38 @@ bot.on(Events.MessageCreate, (msg) => {
   if (msg.content.startsWith(prefix)) {
     const args = msg.content.slice(prefix.length).trim().split(' ');
     let difficulty = -1;
+    let amount = 1;
 
     if (typeof args[0] != undefined) {
       if (args[0].toLowerCase() === 'help') {
         msg.channel.send(
           '```Usage:\n\n\t!problem (without args) - gives you a random problem of any difficulty either paid/free.' +
-          '\n\nAdding difficulty modifiers:\n\n\t!problem <easy | medium | hard> - lets you pick a random free problem of the chosen difficulty.```',
+          '\n\nAdding difficulty modifiers:\n\n\t!problem <easy | medium | hard> - lets you pick a random free problem of the chosen difficulty.' +
+          '\n\nAdding amount modifier:\n\n\t!problem <easy | medium | hard> <1-4> - lets you pick a specified number of random free problem (max 4) of the chosen difficulty.```',
         );
 
         return;
       }
 
-      if (levels.indexOf(args[0].toLowerCase()) > -1) {
+      if (levels.indexOf(args[0].toLowerCase()) > -1 && Number(args[0]) !== 'NaN') {
         difficulty = args[0].toLowerCase();
+      }
+
+      if (levels.indexOf(args[1].toLowerCase()) > -1 && Number(args[1]) !== 'NaN') {
+        difficulty = args[1].toLowerCase();
+      }
+
+
+      if (Number(args[0])) {
+        amount = Math.min(Number(args[0]), 4);
+      }
+
+      if (Number(args[1])) {
+        amount = Math.min(Number(args[1]), 4);
       }
     }
 
-    sendProblem(msg, difficulty);
+    sendProblems(msg, difficulty, amount);
   }
 });
 
